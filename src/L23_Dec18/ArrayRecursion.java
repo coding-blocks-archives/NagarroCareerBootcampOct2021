ae67c3f79b38ad79f30b5d5bdcbe306a8922761d
Garima Chhikara <garima@Garimas-MacBook-Pro.local>
2021-12-19 22:54:35 +0530

    Recursion

--- src/L23_Dec18/ArrayRecursion.java
+++ src/L23_Dec18/ArrayRecursion.java
@@ -4,8 +4,8 @@ public class ArrayRecursion {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
-		displayArray(arr, 0);
+		int[] arr = { 10, 20, 30, 80, 15, 30, 60 };
+		System.out.println(lastOcc(arr, 0, 30));
 
 	}
 
@@ -26,4 +26,67 @@ public class ArrayRecursion {
 
 	}
 
+	// 50 40 30 20 10
+	public static void reverseDisplayArray(int[] arr, int idx) {
+
+		if (idx == arr.length)
+			return;
+
+		// 50 40 30 20
+		reverseDisplayArray(arr, idx + 1);
+
+		// 10
+		System.out.print(arr[idx] + " ");
+
+	}
+
+	public static int maxArray(int[] arr, int idx) {
+
+		if (idx == arr.length)
+			return Integer.MIN_VALUE;
+
+		// idx + 1 -> end max
+		int spmax = maxArray(arr, idx + 1);
+
+		// max (sp max , arr[idx])
+		int bpmax = Math.max(arr[idx], spmax);
+
+		return bpmax;
+
+	}
+
+	public static int firstOcc(int[] arr, int idx, int item) {
+
+		if (idx == arr.length)
+			return -1;
+
+		if (arr[idx] == item) {
+			return idx;
+		}
+
+		return firstOcc(arr, idx + 1, item);
+
+	}
+
+	public static int lastOcc(int[] arr, int idx, int item) {
+
+		if (idx == arr.length)
+			return -1;
+
+		int sp = lastOcc(arr, idx + 1, item) ;
+				
+		if (arr[idx] == item && sp == -1) {
+			return idx ;
+		}
+
+		return sp;
+
+	}
+
 }
+
+
+
+
+
+
